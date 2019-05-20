e97ce493d0e08d6bebf818afbdaf0bf95f283dd4
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-20 22:03:58 +0900

    テスト用のボタンの追加

--- ColorCombinationTester/app/src/main/java/com/amebaownd/pikohan_nwiatori/colorcombinationtester/MainActivity.kt
+++ ColorCombinationTester/app/src/main/java/com/amebaownd/pikohan_nwiatori/colorcombinationtester/MainActivity.kt
@@ -2,11 +2,20 @@ package com.amebaownd.pikohan_nwiatori.colorcombinationtester
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.amebaownd.pikohan_nwiatori.colorcombinationtester.View.CircleSelector
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var circleSelector:CircleSelector
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        circleSelector = findViewById<CircleSelector>(R.id.circle_selector)
+        val rand = Random()
+
+        findViewById<Button>(R.id.button2).setOnClickListener{
+            circleSelector.update(rand.nextInt(100).toFloat())
+        }
     }
 }
