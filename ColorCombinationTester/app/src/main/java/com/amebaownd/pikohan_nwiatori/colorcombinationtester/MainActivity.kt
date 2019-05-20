565820c4b10356e098e542fe00c6fb4f30526921
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-20 22:13:21 +0900

    シークバーの追加

--- ColorCombinationTester/app/src/main/java/com/amebaownd/pikohan_nwiatori/colorcombinationtester/MainActivity.kt
+++ ColorCombinationTester/app/src/main/java/com/amebaownd/pikohan_nwiatori/colorcombinationtester/MainActivity.kt
@@ -3,6 +3,7 @@ package com.amebaownd.pikohan_nwiatori.colorcombinationtester
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.SeekBar
 import com.amebaownd.pikohan_nwiatori.colorcombinationtester.View.CircleSelector
 import java.util.*
 
@@ -17,5 +18,22 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button2).setOnClickListener{
             circleSelector.update(rand.nextInt(100).toFloat())
         }
+
+        findViewById<SeekBar>(R.id.seekBar).setOnSeekBarChangeListener(seekBarChangeListener(circleSelector))
+    }
+
+    private class seekBarChangeListener(private val circleSelector: CircleSelector):SeekBar.OnSeekBarChangeListener{
+        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+            circleSelector.update(progress.toFloat())
+        }
+
+        override fun onStartTrackingTouch(p0: SeekBar?) {
+
+        }
+
+        override fun onStopTrackingTouch(p0: SeekBar?) {
+
+        }
+
     }
 }
